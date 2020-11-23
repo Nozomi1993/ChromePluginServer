0a4454de121955ee9c528904868b84abd92d7779
duqian <349974952@qq.com>
2020-11-23 11:27:35 +0800

    submit

--- src/main/java/com/example/demo/ServerApplication.java
+++ src/main/java/com/example/demo/ServerApplication.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(value = "com.example.demo.entity")
+@MapperScan("com.example.demo.mapper")
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-		System.out.println("项目启动成功");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+        System.out.println("项目启动成功");
+    }
 
 }
