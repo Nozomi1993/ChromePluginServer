687faf02203e835cc6af1b4d9a26493748cebebf
duqian <349974952@qq.com>
2020-11-23 16:08:45 +0800

    submit

---
+++ src/main/java/com/example/demo/config/MySqlInjector.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+
+import java.util.List;
+
+/**
+ * 自定义Sql注入
+ *
+ * @author nieqiurong 2018/8/11 20:23.
+ */
+public class MySqlInjector extends DefaultSqlInjector {
+
+    @Override
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass) {
+        List<AbstractMethod> methodList = super.getMethodList(mapperClass);
+        return methodList;
+    }
+
+}
---
+++ src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author duqian
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.example.demo.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+
+    @Bean
+    public MySqlInjector sqlInjector() {
+        return new MySqlInjector();
+    }
+
+}
